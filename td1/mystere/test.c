971252ea1df99068dad1b847a271f25736abf046
jmarzin1 <jmarzin@enseirb-matmeca.fr>
2016-01-30 20:57:21 +0100

    maj dernier exo

--- td1/mystere/test.c
+++ td1/mystere/test.c
@@ -1,19 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "mystere.h"
+#include <string.h>
+
+void test_upcase_word(char* arg1, char* arg2, char* res) {
+  upcase_word(arg1);
+  upcase_word_2(arg2);
+  if (strcmp(arg1, res) == 0) {
+    printf("upcase_word OK\n");
+  }
+  else {
+    printf("Found : %s, Expected : %s\n", arg1, res);
+    printf("upcase_word FAILED\n");
+  }
+  
+  if (strcmp(arg2, res) == 0) {
+    printf("upcase_word_2 OK\n");
+  }
+  else {
+    printf("Found : %s, Expected : %s\n", arg2, res);
+    printf("upcase_word_2 FAILED\n");
+  }
+}
+
+
 
 int main(int argc, char **argv) {
-  /*test de upcase_word*/
-  /*char test[] = "PAUPAU PauPau  -titi";
-  char test2[] = "PAUPAU PauPau  -titi";
-  printf("%s\n", test);
-  int result = upcase_word(test);
-  upcase_word_2(test2);
-  printf("%s\n", test);
-  printf("%s\n", test2);*/
 
+  /*test upcase*/
+  if (argc == 4) {
+    test_upcase_word(argv[1], argv[2], argv[3]);
+  }
+  
   /* test de months*/
-  /*for (int i=1; i < 14; i++) {
+  /*for (int i=1; i < 13; i++) {
     printf("%d\n", i);
     const char* res = malloc(sizeof(months(i)));
     res = months(i);
