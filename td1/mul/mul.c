63b0361b527b805faa46fcd47f7f981a12a8567e
jmarzin1 <jmarzin@enseirb-matmeca.fr>
2016-01-31 13:47:28 +0100

    ajout peut-être pas nécessaire

--- td1/mul/mul.c
+++ td1/mul/mul.c
@@ -1,6 +1,20 @@
 int mul(int a, int b) {
   int m = 0;
-
+  int neg=0;
+  
+  if (a<0 && b<0) {
+    a*=-1;
+    b*=-1;
+  }
+  else if (a<0) {
+    a*= -1;
+    neg = 1;
+  }
+  else if (b<0) {
+    b*=-1;
+    neg = 1;
+  }
+  
   while (b > 0) {
     if (b%2 ==1) {
       m = m+a;
@@ -10,5 +24,8 @@ int mul(int a, int b) {
     b = b/2;
   }
 
-  return m;
+  if (neg)
+    return -m;
+  else 
+    return m;
 }
